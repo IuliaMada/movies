fe7ba2530b644baef7c025456443e3072316d3f5
IuliaMada <iulia.valeria.md@gmail.com>
2015-02-23 18:49:59 +0200

    Add search form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,11 @@
 class MoviesController < ApplicationController
   def index
+  	
+  	# @respons = HTTParty.get("http://myapifilms.com/imdb?title=#{params[:movie_title].gsub(' ', '%20')}&limit=10&year=#{params[:year]}&filter=#{params[:category]}")
+  	response = HTTParty.get("http://myapifilms.com/imdb?title=#{params[:movie_title].gsub(' ', '%20')}&limit=10")
+  	# @result = HTTParty::Parser.call(response.body, :json)
+      @movies_result = JSON.parse(response)
+   
+
   end
 end
