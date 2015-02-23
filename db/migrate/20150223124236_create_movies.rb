396e182b9ca1b07acd40134ff284d2f98ab9bef5
IuliaMada <iulia.valeria.md@gmail.com>
2015-02-23 15:23:13 +0200

    First commit

---
+++ db/migrate/20150223124236_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :poster
+      t.integer :year
+      t.string :duration
+      t.integer :imdb_rating
+
+      t.timestamps null: false
+    end
+  end
+end
